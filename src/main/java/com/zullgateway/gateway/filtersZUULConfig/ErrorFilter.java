53d10a92466551e07ce9359d7dfbab1528a34594
Abhishek Singh <abhishekit13006@gmail.com>
2021-12-11 17:51:44 +0530

    Initial commit

---
+++ src/main/java/com/zullgateway/gateway/filtersZUULConfig/ErrorFilter.java
@@ -0,0 +1,39 @@
+package com.zullgateway.gateway.filtersZUULConfig;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+@Component
+public class ErrorFilter extends ZuulFilter {
+    private static final Logger log = LoggerFactory.getLogger(ErrorFilter.class);
+
+    @Override
+    public String filterType() {
+        return "error";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        HttpServletResponse response = RequestContext.getCurrentContext().getResponse();
+
+        log.info("ErrorFilter: " + String.format("response status is %d", response.getStatus()));
+
+        return null;
+    }
+}
---
+++ src/main/java/com/zullgateway/gateway/filtersZUULConfig/PostFilter.java
@@ -0,0 +1,38 @@
+package com.zullgateway.gateway.filtersZUULConfig;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class PostFilter extends ZuulFilter {
+    private static Logger log = LoggerFactory.getLogger(PostFilter.class);
+    @Override
+    public String filterType() {
+        return "post";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        HttpServletResponse response = RequestContext.getCurrentContext().getResponse();
+
+        log.info("PostFilter: " + String.format("response's content type is %s", response.getStatus()));
+
+        return null;
+    }
+}
